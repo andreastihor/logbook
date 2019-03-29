4d4b1dff6b69a1c520cc33fc3c0c479b6847e613
Andreas <t-andreas.tihor@ovo.id>
2019-03-29 15:02:11 +0700

    remove sequelize

--- handler.js
+++ handler.js
@@ -1,10 +1,19 @@
 const lineServer = require('./lineServer')
-
+const mysql = require('mysql')
+const connection = mysql.createConnection({
+  host: 'https://miami.viewen.com:2083',
+  user : 'andreast_logbook',
+  password : 'andre151098',
+  database : 'andreast_logbook'
+})
 
 
   function handleEvent (event) {
     if (event.message.text == "test") {
       return testing(event)
+    }
+    if (event.message.text == "user") {
+      return user(event)
     }
     return lineServer.replyMessage(event.replyToken, {
       type: 'text',
@@ -12,11 +21,22 @@ const lineServer = require('./lineServer')
     });
   }
 
-  function username(message) {
-
+  function user(event) {
+    let user = []
+    connection.query('SELECT * from user',(err,row,fields) => {
+      if (!err) {
+      }
+      console.log(err);
+    })
+    connection.close()
+    return lineServer.replyMessage(event.replyToken, {
+      type: 'text',
+      text: user.toString()
+    });
   }
 
   function testing(event) {
+
     return lineServer.replyMessage(event.replyToken, {
       type: 'text',
       text: "apalo"
