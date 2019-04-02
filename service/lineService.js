3de944b6cd877d6774de03f00330d7dd2a3f017c
Andreas <t-andreas.tihor@ovo.id>
2019-04-02 12:19:48 +0700

    fix bug

--- service/lineService.js
+++ service/lineService.js
@@ -121,8 +121,7 @@ async function sendData(event) {
   data.password = password
 
   const pesan = await logbook(data)
-  console.log(data);
-  message(event,`${pesan} with \n in :${data.in} \n out: ${data.out} \n activity: ${data.activity} \n description: ${data.description}`)
+  return message(event,`${pesan} with \n in :${data.in} \n out: ${data.out} \n activity: ${data.activity} \n description: ${data.description}`)
 }
 
 function message(event,message) {
