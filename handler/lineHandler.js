7e7cdf010d491d9e62a67a774b487e40e5e723bc
Andreas <t-andreas.tihor@ovo.id>
2019-04-08 16:50:47 +0700

    fix bug

--- handler/lineHandler.js
+++ handler/lineHandler.js
@@ -81,7 +81,7 @@ const lineController = require('../controller/lineController')
       return lineController.getData(event)
     }
 
-    if (event.message.text.toLowerCase().split(',')[0] == "data") {
+    if (event.message.text.toLowerCase().split('|')[0] == "data") {
       let data = event.message.text.split('|')
       data.shift()
       if (data.length !=4 || ( data[0] == undefined || data[1] == undefined || data[2] == undefined || data[3] == undefined) || (data[0] == "" || data[1] == "" || data[2] == "" || data[3] == "") ) {
